c35426c312c66115723c30feaf5b7dafd316839a
Arthur Pate <arthurpate42@gmail.com>
2023-02-09 13:27:24 -0800

    Update and delete routes added

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const Plant = require('../models/plant')
+const { findByIdAndDelete } = require('../models/region')
 
 const createPlant = async (req, res) => {
     try {
@@ -41,10 +42,34 @@ const getPlantById = async (req, res) => {
         })
     } 
 }
+const updatePlant = async (req, res) => {
+    try {
+    const plant = await Plant.findByIdAndUpdate(req.params.id, req.body, {new: true})
+     res.status(200).json(plant) 
+    } catch(error) {
+        return res.status(500).json({
+            error: error.message
+        })
+    } 
+}
 
+const deletePlant = async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleted = findByIdAndDelete(id)
+        if(deleted) {
+            return res.status(200).send('Plant deleted')
+        } 
+        throw new Error("Plant not found")
+    } catch(error) {
+        return res.status(500).send(error.message)
+    }
+}
 
 module.exports = {
     createPlant,
     getPlants,
-    getPlantById
+    getPlantById,
+    updatePlant,
+    deletePlant
 }
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => res.send('This is root!'))
 router.post('/plants', controllers.createPlant)
 router.get('/plants', controllers.getPlants)
 router.get('/plants/:id', controllers.getPlantById)
-
+router.put('/plants/:id', controllers.updatePlant)
+router.delete('plants/:id', controllers.deletePlant)
 
 module.exports = router;
